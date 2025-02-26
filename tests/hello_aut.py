58c318aa148f83cb269cda0ffc9bc9ad34775323
Siddiq Odiq <94227455+siddiqodiq@users.noreply.github.com>
2025-02-26 09:57:05 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -6,32 +6,42 @@ from concurrent.futures import ThreadPoolExecutor
 
 class AutTest(unittest.TestCase):
     def setUp(self):
+        # Daftar browser yang akan diuji
         self.browsers = {
             'firefox': webdriver.FirefoxOptions(),
             'chrome': webdriver.ChromeOptions(),
             'edge': webdriver.EdgeOptions()
         }
-        for browser, options in self.browsers.items():
+        # Konfigurasi opsi untuk setiap browser
+        for options in self.browsers.values():
             options.add_argument('--ignore-ssl-errors=yes')
             options.add_argument('--ignore-certificate-errors')
         self.server = 'http://localhost:4444'
 
     def test_homepage(self, browser_name):
+        # Mengambil opsi browser yang sesuai
         options = self.browsers[browser_name]
+        # Membuat instance browser remote
         browser = webdriver.Remote(command_executor=self.server, options=options)
         self.addCleanup(browser.quit)
 
+        # Membuka URL
         if len(sys.argv) > 1:
             url = sys.argv[1]
         else:
             url = "http://localhost"
         browser.get(url)
+
+        # Mengambil screenshot
         browser.save_screenshot(f'screenshot_{browser_name}.png')
+
+        # Memeriksa teks yang diharapkan
         expected_result = "Welcome back, Guest!"
         actual_result = browser.find_element(By.TAG_NAME, 'p')
         self.assertIn(expected_result, actual_result.text)
 
     def test_parallel(self):
+        # Menjalankan tes secara paralel untuk setiap browser
         with ThreadPoolExecutor(max_workers=3) as executor:
             futures = [executor.submit(self.test_homepage, browser_name) for browser_name in self.browsers.keys()]
             for future in futures:
