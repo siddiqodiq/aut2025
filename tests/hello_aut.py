7267a819e81506ab07064d8dcd8fbeb21cb578b8
Siddiq Odiq <94227455+siddiqodiq@users.noreply.github.com>
2025-02-26 09:43:19 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -17,6 +17,7 @@ class AutTest(unittest.TestCase):
         else:
             url = "http://localhost"
         self.browser.get(url)
+        self.browser.save_screenshot('screenshot.png')
         expected_result = "Welcome back, Guest!"
         actual_result = self.browser.find_element(By.TAG_NAME, 'p')
         self.assertIn(expected_result, actual_result.text)
