be88f3e413416a027df1b507475deb769a0e8441
Siddiq Odiq <94227455+siddiqodiq@users.noreply.github.com>
2025-02-26 09:50:50 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -1,26 +1,41 @@
-import unittest, sys
+import unittest
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from concurrent.futures import ThreadPoolExecutor
 
 class AutTest(unittest.TestCase):
     def setUp(self):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        server = 'http://localhost:4444'
-        self.browser = webdriver.Remote(command_executor=server, options=options)
-        self.addCleanup(self.browser.quit)
+        self.browsers = {
+            'firefox': webdriver.FirefoxOptions(),
+            'chrome': webdriver.ChromeOptions(),
+            'edge': webdriver.EdgeOptions()
+        }
+        for browser, options in self.browsers.items():
+            options.add_argument('--ignore-ssl-errors=yes')
+            options.add_argument('--ignore-certificate-errors')
+        self.server = 'http://localhost:4444'
 
-    def test_homepage(self):
+    def test_homepage(self, browser_name):
+        options = self.browsers[browser_name]
+        browser = webdriver.Remote(command_executor=self.server, options=options)
+        self.addCleanup(browser.quit)
+
         if len(sys.argv) > 1:
             url = sys.argv[1]
         else:
             url = "http://localhost"
-        self.browser.get(url)
-        self.browser.save_screenshot('screenshot.png')
+        browser.get(url)
+        browser.save_screenshot(f'screenshot_{browser_name}.png')
         expected_result = "Welcome back, Guest!"
-        actual_result = self.browser.find_element(By.TAG_NAME, 'p')
+        actual_result = browser.find_element(By.TAG_NAME, 'p')
         self.assertIn(expected_result, actual_result.text)
+
+    def test_parallel(self):
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            futures = [executor.submit(self.test_homepage, browser_name) for browser_name in self.browsers.keys()]
+            for future in futures:
+                future.result()
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
