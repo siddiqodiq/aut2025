fb320aa86ca974833ae7105c18c67b431a63a87d
Siddiq Odiq <siddiqodiq@gmail.com>
2025-02-26 10:25:01 +0700

    add yaml tests

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -4,30 +4,29 @@ from selenium.webdriver.common.by import By
 
 class AutTest(unittest.TestCase):
     def setUp(self):
-        browser_type = sys.argv[1] if len(sys.argv) > 1 else 'firefox'
-        
+        browser = sys.argv[2] if len(sys.argv) > 2 else "firefox"
         options = None
-        if browser_type == 'firefox':
+
+        if browser == "firefox":
             options = webdriver.FirefoxOptions()
-        elif browser_type == 'chrome':
+        elif browser == "chrome":
             options = webdriver.ChromeOptions()
-        elif browser_type == 'edge':
+        elif browser == "edge":
             options = webdriver.EdgeOptions()
 
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
-        
-        server = f'http://localhost:444{["4", "5", "6"].index(browser_type)}'
+        server = 'http://localhost:4444'
         self.browser = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
-        if len(sys.argv) > 2:
-            url = sys.argv[2]
+        if len(sys.argv) > 1:
+            url = sys.argv[1]
         else:
             url = "http://localhost"
         self.browser.get(url)
-        self.browser.save_screenshot(f'screenshot_{sys.argv[1]}.png')
+        self.browser.save_screenshot(f'screenshot_{sys.argv[2]}.png')
         expected_result = "Welcome back, Guest!"
         actual_result = self.browser.find_element(By.TAG_NAME, 'p')
         self.assertIn(expected_result, actual_result.text)
