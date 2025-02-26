f5b89bf13a4f5ecfaf6d576c466f496c46e7eed7
Siddiq Odiq <94227455+siddiqodiq@users.noreply.github.com>
2025-02-26 10:16:32 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -4,20 +4,30 @@ from selenium.webdriver.common.by import By
 
 class AutTest(unittest.TestCase):
     def setUp(self):
-        options = webdriver.FirefoxOptions()
+        browser_type = sys.argv[1] if len(sys.argv) > 1 else 'firefox'
+        
+        options = None
+        if browser_type == 'firefox':
+            options = webdriver.FirefoxOptions()
+        elif browser_type == 'chrome':
+            options = webdriver.ChromeOptions()
+        elif browser_type == 'edge':
+            options = webdriver.EdgeOptions()
+
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
-        server = 'http://localhost:4444'
+        
+        server = f'http://localhost:444{["4", "5", "6"].index(browser_type)}'
         self.browser = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
-        if len(sys.argv) > 1:
-            url = sys.argv[1]
+        if len(sys.argv) > 2:
+            url = sys.argv[2]
         else:
             url = "http://localhost"
         self.browser.get(url)
-        self.browser.save_screenshot('screenshot.png')
+        self.browser.save_screenshot(f'screenshot_{sys.argv[1]}.png')
         expected_result = "Welcome back, Guest!"
         actual_result = self.browser.find_element(By.TAG_NAME, 'p')
         self.assertIn(expected_result, actual_result.text)
