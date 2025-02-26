49cda3aa5f183b98db23c62a90b6a21442c6a9e5
Siddiq Odiq <94227455+siddiqodiq@users.noreply.github.com>
2025-02-26 10:06:48 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -2,7 +2,6 @@ import unittest
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from concurrent.futures import ThreadPoolExecutor
 
 class AutTest(unittest.TestCase):
     def setUp(self):
@@ -18,34 +17,28 @@ class AutTest(unittest.TestCase):
             options.add_argument('--ignore-certificate-errors')
         self.server = 'http://localhost:4444'
 
-    def test_homepage(self, browser_name):
-        # Mengambil opsi browser yang sesuai
-        options = self.browsers[browser_name]
-        # Membuat instance browser remote
-        browser = webdriver.Remote(command_executor=self.server, options=options)
-        self.addCleanup(browser.quit)
+    def test_homepage(self):
+        # Jalankan tes untuk setiap browser
+        for browser_name, options in self.browsers.items():
+            with self.subTest(browser=browser_name):
+                # Membuat instance browser remote
+                browser = webdriver.Remote(command_executor=self.server, options=options)
+                self.addCleanup(browser.quit)
 
-        # Membuka URL
-        if len(sys.argv) > 1:
-            url = sys.argv[1]
-        else:
-            url = "http://localhost"
-        browser.get(url)
+                # Membuka URL
+                if len(sys.argv) > 1:
+                    url = sys.argv[1]
+                else:
+                    url = "http://localhost"
+                browser.get(url)
 
-        # Mengambil screenshot
-        browser.save_screenshot(f'screenshot_{browser_name}.png')
+                # Mengambil screenshot
+                browser.save_screenshot(f'screenshot_{browser_name}.png')
 
-        # Memeriksa teks yang diharapkan
-        expected_result = "Welcome back, Guest!"
-        actual_result = browser.find_element(By.TAG_NAME, 'p')
-        self.assertIn(expected_result, actual_result.text)
-
-    def test_parallel(self):
-        # Menjalankan tes secara paralel untuk setiap browser
-        with ThreadPoolExecutor(max_workers=3) as executor:
-            futures = [executor.submit(self.test_homepage, browser_name) for browser_name in self.browsers.keys()]
-            for future in futures:
-                future.result()
+                # Memeriksa teks yang diharapkan
+                expected_result = "Welcome back, Guest!"
+                actual_result = browser.find_element(By.TAG_NAME, 'p')
+                self.assertIn(expected_result, actual_result.text)
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
